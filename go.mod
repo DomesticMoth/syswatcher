3615c06f8b5b129c25da12a20d72de7c406377f6
John Doe <>
2022-02-08 08:49:25 +0000

    Test

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/DomesticMoth/syswatcher
+
+go 1.16
+
+require (
+	github.com/ClickHouse/clickhouse-go v1.5.3 // indirect
+	github.com/ClickHouse/clickhouse-go/v2 v2.0.8 // indirect
+	github.com/DomesticMoth/confer v0.0.0-20220114204522-67a669dc3dc3 // indirect
+	github.com/c9s/goprocinfo v0.0.0-20210130143923-c95fcf8c64a8 // indirect
+	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"time"
+	"context"
+	log "github.com/sirupsen/logrus"
+	linuxproc "github.com/c9s/goprocinfo/linux"
+	"github.com/pbnjay/memory"
+	"github.com/ClickHouse/clickhouse-go/v2"
+)
+
+type Conf struct{
+	Delay uint64
+	Addr string
+	Database string
+	Username string
+	Password string
+}
+
+func calcSingleCoreUsage(curr, prev linuxproc.CPUStat) float32 {
+  PrevIdle := prev.Idle + prev.IOWait
+  Idle := curr.Idle + curr.IOWait
+  PrevNonIdle := prev.User + prev.Nice + prev.System + prev.IRQ + prev.SoftIRQ + prev.Steal
+  NonIdle := curr.User + curr.Nice + curr.System + curr.IRQ + curr.SoftIRQ + curr.Steal
+  PrevTotal := PrevIdle + PrevNonIdle
+  Total := Idle + NonIdle
+  totald := Total - PrevTotal
+  idled := Idle - PrevIdle
+  CPU_Percentage := (float32(totald) - float32(idled)) / float32(totald)
+  return CPU_Percentage
+}
+
+func getRam() uint64{
+	total := memory.TotalMemory()
+	free := memory.FreeMemory()
+	return (total-free)/(total/100)
+}
+
+func getCpu() linuxproc.CPUStat {
+	stat, err := linuxproc.ReadStat("/proc/stat")
+	if err != nil { log.Fatal(err) }
+	return stat.CPUStatAll
+}
+
+func main(){
+	var conf Conf
+	conf.Delay = 1000
+	conf.Addr = "127.0.0.1:9000"
+	conf.Database = "Hostonfo"
+	conf.Username = "default"
+	conf.Password = ""
+
+	ctx := context.Background()
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{conf.Addr},
+		Auth: clickhouse.Auth{
+			Database: conf.Database,
+			Username: conf.Username,
+			Password: conf.Password,
+		},
+		DialTimeout:     time.Second,
+		MaxOpenConns:    10,
+		MaxIdleConns:    5,
+		ConnMaxLifetime: time.Hour,
+	})
+	if err != nil { log.Fatal(err) }
+	
+	last := getCpu()
+	for {
+		stat := getCpu()
+		load := uint64(calcSingleCoreUsage(stat, last)*100)
+		last = stat
+		if load < 100 {
+			log.Info(load, getRam())
+			err := conn.AsyncInsert(ctx, "INSERT INTO System VALUES (0,0,0,0)", false)
+			if err != nil { log.Fatal(err) }
+		}
+		time.Sleep(time.Duration(conf.Delay) * time.Millisecond)
+	}
+}
